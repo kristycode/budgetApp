2889ffdaec626830d162014d323516765a867f9b
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-06-01 17:24:05 -0500

    Updated server.js again to try to get mongodb to work with heroku deployment

--- server.js
+++ server.js
@@ -15,13 +15,12 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE || "mongodb://localhost/budget";
-
-mongoose.connect( MONGODB_URI, {
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true
+  useUnifiedTopology: true,
 });
+
 
 app.use(require('./routes/api.js'))
 
