348c6f5d415c2450b0192ff7de51613556518ae7
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-06-01 17:38:39 -0500

    Still trying to get Heroku deployment to work, checked logs and it has an error with Heroku failing to bind the port

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const logger = require('morgan')
 const mongoose = require('mongoose')
 const compression = require('compression')
 
-const PORT = 3000
+const PORT = process.env.PORT || 3000;
 
 const app = express()
 
@@ -15,10 +15,12 @@ app.use(express.json())
 
 app.use(express.static('public'))
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/budget', {
+var MONGODB_URI = process.env.MONGODB_URI || process.env.REMOTE || "mongodb://localhost/budget";
+
+mongoose.connect( MONGODB_URI, {
   useNewUrlParser: true,
   useFindAndModify: false,
-  useUnifiedTopology: true,
+  useUnifiedTopology: true
 });
 
 app.use(require('./routes/api.js'))
