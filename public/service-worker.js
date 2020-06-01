71b12166955b3216ad9b5108cddfd26481fd3633
Kristy Farmer <kristyfarmerschool@gmail.com>
2020-06-01 16:41:42 -0500

    Forgot to save a couple of files after formatting

--- public/service-worker.js
+++ public/service-worker.js
@@ -16,7 +16,6 @@ const DATA_CACHE_NAME = 'data-cache-v1'
 self.addEventListener('install', function (evt) {
   evt.waitUntil(
     caches.open(CACHE_NAME).then(cache => {
-      console.log('Your files were pre-cached successfully!')
       return cache.addAll(FILES_TO_CACHE)
     })
   )
@@ -30,7 +29,6 @@ self.addEventListener('activate', function (evt) {
       return Promise.all(
         keyList.map(key => {
           if (key !== CACHE_NAME && key !== DATA_CACHE_NAME) {
-            console.log('Removing old cache data', key)
             return caches.delete(key)
           }
         })
